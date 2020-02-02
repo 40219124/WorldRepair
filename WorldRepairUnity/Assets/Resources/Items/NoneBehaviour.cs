6d42f7c9c0e8ac2ba05b47e267cf2ceb2186126f
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-02-02 14:00:10 +0000

    ✨ Added upgrading bugs into a mouse

---
+++ WorldRepairUnity/Assets/Resources/Items/NoneBehaviour.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class NoneBehaviour : BehaviourObj
+{
+    public ItemTemplate Template;
+
+    public override IEnumerator<YieldInstruction> Run(PlayerController player, Interactable target)
+    {
+        yield break;
+    }
+}
