7bb900ce2ed6f6ade1a97505bab58112d5c19aca
Anthony Marmont <dev.anthonymarmont@gmail.com>
2020-02-07 21:41:55 +0000

    🐛 Fixed sorting issues, always sort on the z-axis

---
+++ WorldRepairUnity/Assets/Shaders/SortTheSprites.cs
@@ -0,0 +1,24 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+using UnityEngine;
+using UnityEngine.Rendering;
+
+#if UNITY_EDITOR
+[InitializeOnLoad]
+#endif
+public class SortTheSprites : MonoBehaviour
+{
+    static SortTheSprites()
+    {
+        OnLoad();
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void OnLoad()
+    {
+        GraphicsSettings.transparencySortMode = TransparencySortMode.CustomAxis;
+        GraphicsSettings.transparencySortAxis = new Vector3(0.0f, 0.0f, 1.0f);
+    }
+}
